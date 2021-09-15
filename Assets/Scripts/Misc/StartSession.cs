bde58d4f5256cdd041fed71eeff00042559ed030
doCent90 <78255120+doCent90@users.noreply.github.com>
2021-09-15 20:29:57 +0300

    Added levels

--- Assets/Scripts/Misc/StartSession.cs
+++ Assets/Scripts/Misc/StartSession.cs
@@ -3,19 +3,29 @@ using IJunior.TypedScenes;
 
 public class StartSession : MonoBehaviour
 {
-    [SerializeField] 
-
-    private int _sessionCount = 0;
-
     private const string SessionCount = "Session_Count";
-    private const string FirstDate = "Reg_Day";
-    private const string DaysInGame = "Days_In_Game";
 
     private void Awake()
     {
-        _sessionCount++;
+        Debug.Log(PlayerPrefs.GetInt(SessionCount));
+        //Init();
 
-        if (PlayerPrefs.GetInt(SessionCount) != _sessionCount)
-            PlayerPrefs.SetInt(SessionCount, _sessionCount);
+        int sessionCount = PlayerPrefs.GetInt(SessionCount);
+
+        if (sessionCount != 0)
+        {
+            sessionCount++;
+            PlayerPrefs.SetInt(SessionCount, sessionCount);
+        }
+
+        LVL1.Load();
+    }
+
+    private void Init()
+    {
+        int sessionCount = PlayerPrefs.GetInt(SessionCount);
+
+        if (sessionCount <= 0)
+            PlayerPrefs.SetInt(SessionCount, 0);
     }
 }
